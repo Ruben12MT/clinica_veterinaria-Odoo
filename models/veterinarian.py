3ab31210fdadc9016419b191cfba524833bb86b1
Pablo Plazuelo Jiménez <100439645+Kasi800@users.noreply.github.com>
2026-01-27 00:20:46 +0100

    Informes, herencia de vista, campos funcionales y workflow.

--- models/veterinarian.py
+++ models/veterinarian.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Veterinarian(models.Model):
     _name = 'clinica.veterinarian'
@@ -17,3 +18,26 @@ class Veterinarian(models.Model):
     ], string='Especialidad', required=True)
     schedule = fields.Char(string='Horario')
     appointment_ids = fields.One2many('clinica.appointment', 'veterinarian_id', string='Historial de citas', readonly=True)
+    state = fields.Selection([
+        ('available', 'Disponible'),
+        ('busy', 'En Consulta'),
+        ('off', 'De Baja'),
+    ], string='Estado', default='available', tracking=True)
+
+    def action_set_busy(self):
+        self.state = 'busy'
+
+    def action_set_available(self):
+        self.state = 'available'
+
+    def action_set_off(self):
+        self.state = 'off'
+
+    @api.constrains('license_number')
+    def _check_license_format(self):
+        for record in self:
+            if record.license_number and not record.license_number.startswith('LIC-'):
+                raise ValidationError(
+                    "Formato de licencia inválido: El número de colegiado debe "
+                    "comenzar obligatoriamente por 'LIC-'. Ejemplo: LIC-12345"
+                )
